9dfecec9d30ae1cd9c43261aa0b49d3a58dd3f5c
Domantas Martišiūnas <domantasmart04@gmail.com>
2025-03-17 20:33:53 +0200

    Added Map and Contact

---
+++ form-handler.php
@@ -0,0 +1,21 @@
+<?php
+$name=$_POST['name'];
+$visitor_phone=$_POST['phone'];
+$visitor_email=$_POST['email'];
+$message=$_POST['message'];
+
+$email_from='info@MotorMaster.com';
+$email_subject='New Form Submission';
+$email_body ="User Name: $name.\n".
+"User Phone: $visitor_phone.\n".
+"User Email: $visitor_email.\n".
+"User message: $message.\n".
+
+$to = 'domavika48@gmail.com';
+$headers ="From: $email_from \r\n";
+$headers . ="Reply-To: $visitor_from \r\n";
+
+mail($to,$email_subject,$email_body,$headers);
+
+header("Location: contact.html");
+?>
